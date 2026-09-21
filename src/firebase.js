4ec8ab8e592b2e06b14e29431dcc3adcc0818a9c
ONYANGO FEISWAL DINDI <feisaldindi4@gmail.com>
2026-09-21 08:46:09 +0300

    Fix multiple typos in Firebase API key

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore"; // [NEW] Import Database
 
 // YOUR CONFIG (Keep your own keys here!)
 const firebaseConfig = {
-  apiKey: "AIzaSyBsdoUCS5OGRz_0rZnZEj5e8bAFivjdsOw",
+  apiKey: "AIzaSyBsdouCS50GRZ_0rZnZEj5e8bAFivjdsOw",
   authDomain: "ai-readiness-b3883.firebaseapp.com",
   projectId: "ai-readiness-b3883",
   storageBucket: "ai-readiness-b3883.firebasestorage.app",
