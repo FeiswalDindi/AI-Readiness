58b06e7340a73ca63382bc0f2a369838faef64b4
ONYANGO FEISWAL DINDI <feisaldindi4@gmail.com>
2026-09-24 16:32:10 +0300

    fix: rename download button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -379,7 +379,7 @@ onUnmounted(() => {
                     <!-- Download button -->
                     <a :href="store.content.posterUrl" download="Project_Poster.jpg" class="btn btn-outline-dark btn-lg px-4 py-3 rounded-0 fw-bold shadow-sm text-uppercase ls-1 d-flex align-items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-2" viewBox="0 0 16 16"><path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/><path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/></svg>
-                        Download Poster
+                        Download
                     </a>
                 </div>
             </div>
